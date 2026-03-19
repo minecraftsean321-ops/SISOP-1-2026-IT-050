3d5615107e0f8a9b17204a1afb3c64ae6e3e7cd3
minecraftsean321 <minecraftsean321@gmail.com>
2026-03-19 16:55:09 +0700

    soal

--- soal_3/kost_slebew.sh
+++ soal_3/kost_slebew.sh
@@ -1,12 +1,12 @@
 #!/bin/bash
 
-if [[ $1 == --check-tagihan ]]; then 
+if [[ "$1" == "--check-tagihan" ]]; then 
 
-tgl_rekap=$(date "+\%Y-\%m-\%d \%H:\%M")
+tgl_rekap=$(date "+%Y-%m-%d %H:%M")
 
-awk -F "," -v tgl=$tgl_rekap '$5 ~ /Menunggak/ {
-	printf [/%s] "TAGIHAN: <\%s>, (Kamar <\%s>) - Menunggak Rp<\%s>\n", tgl , $1 , $2 , $4}' 
-./z_data/penghuni.csv >> ./z_log/tagihan.log
+awk -F "," -v tgl="$tgl_rekap" '$5 ~ /Menunggak/ {
+	printf "[/%s] TAGIHAN: <%s>, (Kamar <%s>) - Menunggak Rp<%s>\n", tgl , $1 , $2 , $4}' 
+./data/penghuni.csv >> ./log/tagihan.log
 
 exit 0
 
@@ -43,7 +43,7 @@ case $pilihan in
 	while true; do
         read -p "Masukkan Nomor Kamar: " kamar
 	#Memvalidasi agar tidak ada no kamar yang sama 
-	if grep -q "^.*,$kamar" ./z_data/penghuni.csv; then 
+	if grep -q "^.*,$kamar" ./data/penghuni.csv; then 
 	echo -e "\e[31m[!] Error: Kamar $kamar sudah terisi!\e[0m"
 	    else
 	break #Kamar tersedia kelaur dari loop 
@@ -86,7 +86,7 @@ case $pilihan in
 
         read -p "Masukkan Status Awal (Aktif/Menunggak): " status
 
-	echo "$nama,$kamar,$tanggal,$sewa,$status" >> ./z_data/penghuni.csv
+	echo "$nama,$kamar,$tanggal,$sewa,$status" >> ./data/penghuni.csv
 
 	# Menampilkan hasil dengan warna hijau (kode \e[32m)
         echo -e "\e[32m[✓] Penghuni \"$nama\" berhasil ditambahkan ke Kamar \"$kamar\" dengan status \"$status\"\e[0m"
@@ -101,12 +101,12 @@ case $pilihan in
         echo "===================================="
 	read -p "Masukkan nama penghuni yang akan dihapus: " hapusnama
 
-	if ! grep -q "$hapusnama" ./z_data/penghuni.csv; then 
+	if ! grep -q "$hapusnama" ./data/penghuni.csv; then 
 	echo "\e[31m[!] Error: Nama penghuni tidak ditemukan \e[0m"
 
 	else 
 	
-	data_penghuni=$(grep "$hapusnama" ./z_data/penghuni.csv)
+	data_penghuni=$(grep "$hapusnama" ./data/penghuni.csv)
 	
 	echo "Nama penghuni ditemukan!"
 	read -p "Yakin ingin menghapus? (y/n): " konfirmasi
@@ -117,10 +117,10 @@ case $pilihan in
 	tgl_keluar=$(date "+%Y-%m-%d")
 
 	#simpan ke history_hapur
-	echo "$data_penghuni, $tgl_keluar" > ./z_sampah/history_hapus.csv
+	echo "$data_penghuni, $tgl_keluar" > ./sampah/history_hapus.csv
 
 	#hapus dari database utama
-	grep -v "$hapusnama" ./z_data/penghuni.csv > temp.csv && mv temp.csv ./z_data/penghuni.csv
+	sed -i "/$hapusnama/d" ./data/penghuni.csv
 
 	echo -e "\n\e[32m[✓] $hapusnama telah keluar pada $tgl_keluar.\e[0m"
 	echo "[i] Data telah dipindahkan ke history_hapus.csv"
@@ -142,11 +142,11 @@ case $pilihan in
 	echo -e "No | Nama            | Kamar | Harga Sewa     | Status   |"
 	echo "-------------------------------------------------------------"
 
-	awk -F "," 'NR>0 {printf "%d | %s          |  %s  | %s       | %s |\n", NR, $1, $2, $4, $5}' ./z_data/penghuni.csv | column -t -s "|" -o " | "
+	awk -F "," 'NR>0 {printf "%d | %s          |  %s  | %s       | %s |\n", NR, $1, $2, $4, $5}' ./data/penghuni.csv | column -t -s "|" -o " | "
 	echo "--------------------------------------------------------------"
-	penghuni=$(awk 'END {print NR}' ./z_data/penghuni.csv)
-	Aktif=$(awk -F "," ' $5 ~ /Aktif/ {count++} END {print count+0}' ./z_data/penghuni.csv)
-	Menunggak=$(awk -F "," '$5 ~ /Menunggak/ {count++} END {print count+0}' ./z_data/penghuni.csv)
+	penghuni=$(awk 'END {print NR}' ./data/penghuni.csv)
+	Aktif=$(awk -F "," ' $5 ~ /Aktif/ {count++} END {print count+0}' ./data/penghuni.csv)
+	Menunggak=$(awk -F "," '$5 ~ /Menunggak/ {count++} END {print count+0}' ./data/penghuni.csv)
 	echo -e "Total: $penghuni penghuni | Aktif: \e[32m$Aktif\e[0m  | Menunggak: \e[31m$Menunggak\e[0m"
  	echo "============================================================="
 	echo "Tekan [ENTER] untuk kembali ke menu"
@@ -159,14 +159,14 @@ case $pilihan in
         echo "            UPDATE STATUS           "
         echo "===================================="
 	read -p "Masukkan Nama Penghuni: " namabaru
-	if grep -q "$namabaru" ./z_data/penghuni.csv; then
-	awk -F "," -v nama=$namabaru '$1 ~ nama {printf "%s     | %s |%s   | %s \n", $1, $2, $4, $5}' ./z_data/penghuni.csv | column -t -s "|" -o " | "
+	if grep -q "$namabaru" ./data/penghuni.csv; then
+	awk -F "," -v nama=$namabaru '$1 ~ nama {printf "%s     | %s |%s   | %s \n", $1, $2, $4, $5}' ./data/penghuni.csv | column -t -s "|" -o " | "
 	echo "------------------------------------"
 	read -p "Masukkan Status Baru (Aktif/Menunggak): " statusbaru
 	if [ "$statusbaru" == "Aktif" ]; then
-	sed -i "/$namabaru/s/Menunggak/Aktif/" ./z_data/penghuni.csv
+	sed -i "/$namabaru/s/Menunggak/Aktif/" ./data/penghuni.csv
 	elif [ "$statusbaru" == "Menunggak" ]; then
-	sed -i "/$namabaru/s/Aktif/Menunggak/" ./z_data/penghuni.csv
+	sed -i "/$namabaru/s/Aktif/Menunggak/" ./data/penghuni.csv
 	else 
 	echo -e "\e[31m[!] Error: Input Status Salah!\e[0m"
 	
@@ -188,26 +188,26 @@ case $pilihan in
 	echo "=============================================="
         echo "         LAPORAN KEUANGAN KOST SLEBEW         "
         echo "=============================================="
-	if [ ! -s ./z_data/penghuni.csv ]; then
+	if [ ! -s ./data/penghuni.csv ]; then
 	echo -e "\e[31m[!] Data penghuni kosong!\e[0m"
 	else
-	pemasukan=$(awk -F "," '/Aktif/ {sum+=$4} END {print sum+0}' ./z_data/penghuni.csv)
-	tunggakan=$(awk -F "," '/Menunggak/ {sum+=$4} END {print sum+0}' ./z_data/penghuni.csv)
-	jumterisi=$(awk 'END {print NR}' ./z_data/penghuni.csv)
+	pemasukan=$(awk -F "," '/Aktif/ {sum+=$4} END {print sum+0}' ./data/penghuni.csv)
+	tunggakan=$(awk -F "," '/Menunggak/ {sum+=$4} END {print sum+0}' ./data/penghuni.csv)
+	jumterisi=$(awk 'END {print NR}' ./data/penghuni.csv)
 	echo "Total pemasukan (Aktif)   : Rp$pemasukan"
 	echo "Total tunggakan           : Rp$tunggakan"
 	echo "Jumlah kamar terisi       : $jumterisi Kamar"
 	echo "-----------------------------------------------"
 	echo "Daftar penghuni menunggak: "
 	if [ $tunggakan -gt 0 ]; then
-	awk -F "," '$5 ~ /Menunggak/ {printf "%s     | %s |%s   | %s \n", $1, $2, $4, $5}' ./z_data/penghuni.csv | column -t -s "|" -o " | "
+	awk -F "," '$5 ~ /Menunggak/ {printf "%s     | %s |%s   | %s \n", $1, $2, $4, $5}' ./data/penghuni.csv | column -t -s "|" -o " | "
 	else
 	echo "--- Tidak ada tunggakan (SLEBEW!) ---"
 	fi
 	echo "=============================================="
 	tgl_rekap=$(date "+%Y-%m-%d %H:%M")
-	echo "[$tgl_rekap] Pemasukan: $pemasukan,Tunggakan: $tunggakan,Terisi: $jumterisi" >> ./z_rekap/laporan_bulanan.txt
-	echo -e "\n\e[32m[✓] Laporan berhasil disimpan ke rekap ./zz_rekap/laporan_bulanan.txt\e[0m"
+	echo "[$tgl_rekap] Pemasukan: $pemasukan,Tunggakan: $tunggakan,Terisi: $jumterisi" >> ./rekap/laporan_bulanan.txt
+	echo -e "\n\e[32m[✓] Laporan berhasil disimpan ke rekap ./zrekap/laporan_bulanan.txt\e[0m"
 	fi
 	echo "Tekan [ENTER] untuk kembali ke menu"
         read # Menunggu user menekan enter
@@ -237,9 +237,13 @@ read -p "Tekan [ENTER] untuk kembali ke menu"
 ;;
 2)
 	clear
+	#Mengambil alamat absolut
+	SCRIPTH_PATH=$(readlink -f "$0")
+	
+	#Sistem overwrite
 	read -p "Masukkan Jam (00-23): " jam_baru
 	read -p "Masukkan Menit (0-59): " menit_baru
-	(crontab -l 2>/dev/null; echo "$menit_baru $jam_baru * * *  $(readlink -f $0)/kost_slebew.sh --check-tagihan") | crontab -
+	(crontab -l 2>/dev/null | grep -v "$SCRIPTH_PATH"; echo "$menit_baru $jam_baru * * 1-7  $SCRIPTH_PATH --check-tagihan") | crontab -
 	echo -e "\e[32mJadwal berhasil didaftarkan!\e[0m" 	
 	read -p "Tekan [ENTER] untuk kembali ke menu"
 ;;
